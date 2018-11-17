9260fd713c4eab3a9b84d786537c16439cc72a7c
John M. Harris, Jr <johnmh@splentity.com>
2018-11-17 14:40:54 -0500

    Add qrc_resources.cpp to the repo
    Fuck it

--- buildqrc.sh
+++ buildqrc.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Do not invoke this directly
 
-SRCDIR=$1
-RCC=$2
-
-cd $SRCDIR/../rc
-$RCC -o $SRCDIR/qrc_resources.cpp resources.qrc
+cd rc
+rcc-qt5 -o ../src/qrc_resources.cpp resources.qrc
